b25d555a300353e05100550861838daec11df364
Prashant Modukuri <mvr.modukuri@gmail.com>
2017-11-25 15:37:34 -0600

    Add files via upload

---
+++ StartupProject/FirstApi/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Product,Order
+from rest_framework import viewsets
+from FirstApi.serializers import ProductSerializer, OrderSerializer
+
+  
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
